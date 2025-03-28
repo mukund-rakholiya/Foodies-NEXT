8ea50d7da72876979c5fd15f2ce577048882cb80
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-03-28 15:20:40 +0530

    solved some error from community/page.js

--- app/community/page.js
+++ app/community/page.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Image from "next/image";
 import mealIcon from "@/assets/icons/meal.png";
 import communityIcon from "@/assets/icons/community.png";
 import eventsIcon from "@/assets/icons/events.png";
 import classes from "./page.module.css";
 
 function Community() {
-  return (
-    <>
+  return (<>
       <header className={classes.header}>
         <h1>
           One shared passion: <span className={classes.highlight}>Food</span>
