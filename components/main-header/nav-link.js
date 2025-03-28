8a537299a28f0487f42c075fca627e4153311fc1
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-03-28 15:59:48 +0530

    Updated link style

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -11,7 +11,7 @@ function NavLink({ href, children }) {
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? classes.active : undefined}
+      className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link}
     >
       {children}
     </Link>
