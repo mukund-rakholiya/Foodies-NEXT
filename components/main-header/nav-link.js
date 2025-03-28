39201afb04854a6ff9e63362e41f6f0a91320349
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-03-28 15:55:56 +0530

    Move link realated style from 'main-header.module.css' to 'nav-link.module.css'

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -3,6 +3,7 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import React from "react";
+import classes from "./nav-link.module.css";
 
 function NavLink({ href, children }) {
   const path = usePathname();
