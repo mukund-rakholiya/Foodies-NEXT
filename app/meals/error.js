5a98df080456bcab069928657e29ea5337915be4
Mukund Rakholiya <155869925+mukund-rakholiya@users.noreply.github.com>
2025-04-01 20:39:06 +0530

    Update error.js

--- app/meals/error.js
+++ app/meals/error.js
@@ -1,7 +1,7 @@
 'use client';
 
 export default function Error() {
-  return (
+  return ( 
     <main className="error">
       <h1>An error occurred!</h1>
       <p>Failed to fetch meal data. Please try again later.</p>
